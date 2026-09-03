bafc179d408f7e7a95565fbeda765298e0a5da33
shivanshi123444 <parasharshivanshii@gmail.com>
2026-09-03 10:40:13 +0530

    Update frontend API for production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL:  "https://secure-auth-system-84xx.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
